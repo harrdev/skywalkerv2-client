28b1a4fbe5fe32c8d5769d62b35812f595664256
harrdev <jasonharrphx@live.com>
2022-02-12 22:44:29 -0700

    added starship and vehicle details

--- src/components/Details/StarshipDetails.js
+++ src/components/Details/StarshipDetails.js
@@ -1,12 +1,28 @@
 import { useParams } from "react-router-dom";
 
 const StarshipDetails = (props) => {
-    const swdata = useParams()
-    console.log("Props in details: ", props)
+
+    const starshipName = useParams()
+	const starshipData = props.starships.filter(starship => starship.name === starshipName.name)
+	const s = starshipData[0]
 
 	return (
 		<div className="details">
 			<h2>Starship Details Page</h2>
+			<h3>Name: {s.name}</h3>
+			<h3>Model: {s.model}</h3>
+			<h3>Manufacturer: {s.manufacturer}</h3>
+			<h3>Cost in Credits: {s.cost_in_credits}</h3>
+			<h3>Length: {s.length}</h3>
+			<h3>Max Speed: {s.max_atmosphering_speed}</h3>
+			<h3>Crew: {s.crew}</h3>
+			<h3>Passengers: {s.passengers}</h3>
+			<h3>Cargo Capacity: {s.cargo_capacity}</h3>
+			<h3>Consumables: {s.consumables}</h3>
+			<h3>Hyperdrive Rating: {s.hyperdrive_rating}</h3>
+			<h3>MGLT: {s.mglt}</h3>
+			<h3>Starship Class: {s.starship_class}</h3>
+
 		</div>
 	)
 }
--- src/components/Details/VehicleDetails.js
+++ src/components/Details/VehicleDetails.js
@@ -1,12 +1,25 @@
 import { useParams } from "react-router-dom";
 
 const VehicleDetails = (props) => {
-    const swdata = useParams()
-    console.log("Props in details: ", props)
+	
+    const vehicleName = useParams()
+	const vehicleData = props.vehicles.filter(vehicle => vehicle.name === vehicleName.name)
+	const v = vehicleData[0]
 
 	return (
 		<div className="details">
 			<h2>Vehicle Details Page</h2>
+			<h3>Name: {v.name}</h3>
+			<h3>Model: {v.model}</h3>
+			<h3>Manufacturer: {v.manufacturer}</h3>
+			<h3>Cost in Credits: {v.cost_in_credits}</h3>
+			<h3>Length: {v.length}m</h3>
+			<h3>Crew: {v.crew}</h3>
+			<h3>Max Speed: {v.max_atmosphering_speed}</h3>
+			<h3>Passengers: {v.passengers}</h3>
+			<h3>Cargo Capacity: {v.cargo_capacity}</h3>
+			<h3>Consumables: {v.consumables}</h3>
+			<h3>Vehicle Class: {v.vehicle_class}</h3>
 		</div>
 	)
 }
