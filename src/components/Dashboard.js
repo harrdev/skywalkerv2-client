f7cdff61c245ec68523ffd7f44e1096245192ee5
harrdev <jasonharrphx@live.com>
2022-02-16 14:02:31 -0700

    styling dashboard page

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,17 +2,32 @@ import { Link } from 'react-router-dom'
 
 const Dashboard = (props) => {
 
-	return (
-		<div className="dashboard">
-			<h2>Skywalker Academy</h2>
-			<h3><Link to="./People">People</Link></h3>
-			<h3><Link to="./Planets">Planets</Link></h3>
-			<h3><Link to="./Vehicles">Vehicles</Link></h3>
-			<h3><Link to="./Species">Species</Link></h3>
-			<h3><Link to="./Starships">Starships</Link></h3>
-			<h3><Link to="./Films">Films</Link></h3>
-		</div>
-	)
+    return (
+        <div>
+            <h1 className="skywalkerH1">Skywalker Academy</h1>
+            <h3 className="skywalkerH3">Select a category to explore</h3>
+            <div className="containerMain">
+                <div className="itemMain1">
+                    <h3><Link to="./People" className="mainLink">People</Link></h3>
+                </div>
+                <div className="itemMain2">
+                    <h3><Link to="./Planets" className="mainLink">Planets</Link></h3>
+                </div>
+                <div className="itemMain3">
+                    <h3><Link to="./Vehicles" className="mainLink">Vehicles</Link></h3>
+                </div>
+                <div className="itemMain4">
+                    <h3><Link to="./Species" className="mainLink">Species</Link></h3>
+                </div>
+                <div className="itemMain5">
+                    <h3><Link to="./Starships" className="mainLink">Starships</Link></h3>
+                </div>
+                <div className="itemMain6">
+                    <h3><Link to="./Films" className="mainLink">Films</Link></h3>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Dashboard
