448b5b845a35effc62a900361f7ebfa2e27ef680
harrdev <jasonharrphx@live.com>
2022-03-01 23:22:54 -0700

    redirects to login page upon arriving to site

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
+import { useNavigate } from "react-router-dom"
+
 const Home = (props) => {
+	const navigate = useNavigate();
 
 	return (
-		<div className="home" style={{ backgroundImage: "url(https://lumiere-a.akamaihd.net/v1/images/victory-and-death-ahsoka_8e1a6810.jpeg?region=0%2C0%2C1280%2C544)", repeat:"no-repeat", backgroundSize:"cover", backgroundPosition:"center center"}}>
+		<div className="home">
+			{navigate("/sign-in")}
 		</div>
 	)
 }
