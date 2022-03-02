e150c1480c8a559f640845c33f2cebe6847c1a46
harrdev <jasonharrphx@live.com>
2022-03-01 21:04:13 -0700

    fixed home background image position

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 const Home = (props) => {
 
 	return (
-		<div className="home" style={{ backgroundImage: "url(https://lumiere-a.akamaihd.net/v1/images/victory-and-death-ahsoka_8e1a6810.jpeg?region=0%2C0%2C1280%2C544)", repeat:"no-repeat", backgroundSize:"cover"}}>
+		<div className="home" style={{ backgroundImage: "url(https://lumiere-a.akamaihd.net/v1/images/victory-and-death-ahsoka_8e1a6810.jpeg?region=0%2C0%2C1280%2C544)", repeat:"no-repeat", backgroundSize:"cover", backgroundPosition:"center center"}}>
 		</div>
 	)
 }
